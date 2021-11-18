f306fe86f1b0d9fdbc64c21ecff5fceaac7da899
xoxo123456789 <93612328+xoxo123456789@users.noreply.github.com>
2021-11-18 14:44:37 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -46,7 +46,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[© @Mr_D_P]" 
+            performer = f"[@mrSHAGGYboy]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
@@ -67,7 +67,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Uploaded By:</b> <a href="https://t.me/Mr_D_P"> Dark Fm</a>'
+        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Uploaded By:</b> <a href="DMG X"> DMGX Fm</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
